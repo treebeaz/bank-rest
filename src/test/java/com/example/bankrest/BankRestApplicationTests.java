6af1550698f0323bda6a7d83a6b6fa1bd1955e52
treebeaz <kgaraev_3@mail.ru>
2025-11-05 11:42:03 +0300

    init repository

---
+++ src/test/java/com/example/bankrest/BankRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
